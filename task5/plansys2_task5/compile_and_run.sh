8642687d1e93945cb4ba5e6760792dbc8bf70f74
Kevin Depedri <107556051+KevinDepedri@users.noreply.github.com>
2023-01-20 17:40:21 +0100

    Add files via upload

--- task5/plansys2_task5/compile_and_run.sh
+++ task5/plansys2_task5/compile_and_run.sh
@@ -1,4 +1,5 @@
 source /opt/ros/humble/setup.bash
+rosdep install --from-paths ./ --ignore-src -r -y
 colcon build --symlink-install
 source install/setup.bash
 ros2 launch plansys2_task5 plansys2_task5_launch.py
